3ea2041e0fd2e652257708683f3659061f680efe
Rahmatilla1 <xikmatillr@gmail.com>
2025-06-21 20:56:04 +0500

    Initial commit

--- Index.js
+++ Index.js
@@ -1,17 +1,25 @@
+const http = require('http');
 const WebSocket = require('ws');
-const PORT = process.env.PORT || 4001; // Render uchun PORT o‘zgaruvchisi
+const PORT = process.env.PORT || 4001;
 
-const wss = new WebSocket.Server({ port: PORT });
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('WebSocket server is running');
+});
+
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
-   ws.on('message', function incoming(message) {
+  ws.on('message', function incoming(message) {
     console.log('Qabul qilindi:', message.toString());
     wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(message);
-        }
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
     });
   });
 });
 
-console.log(`WebSocket server ${PORT}-portda ishlayapti`);
+server.listen(PORT, () => {
+  console.log(`WebSocket server ${PORT}-portda ishlayapti`);
+});
